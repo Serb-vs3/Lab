58cc48203d291817d7430fc244db86a089798786
Serb-vs3 <vinokyrovsasha9@gmail.com>
2024-10-02 11:18:33 +0300

    Update ONE-DIMENSIONAL ARRAYS (NUMERIC SEQUENCES).cpp

--- ONE-DIMENSIONAL ARRAYS (NUMERIC SEQUENCES).cpp
+++ ONE-DIMENSIONAL ARRAYS (NUMERIC SEQUENCES).cpp
@@ -1,11 +1,39 @@
 #include <iostream>
-
+#include <random>
 int main() {
-    int n;
+    int n, choice;
+    std::cout << "Size:";
     std::cin >> n;
     int* arr = new int[n];
-    for (int i = 0; i < n; ++i) {
-        std::cin >> arr[i];
+    std::cout << "Method:\n";
+    std::cout << "1 - Keyboards\n";
+    std::cout << "2 - Random\n";
+    std::cin >> choice;
+    if (choice == 1) {
+        std::cout << "Enter" << n << " elements:\n";
+        for (int i = 0; i < n; ++i) {
+            std::cin >> arr[i];
+        }
+    } else if (choice == 2) {
+        std::random_device rd;  
+        std::mt19937 gen(rd()); 
+        int min, max;
+        std::cout << "Range (min and max): ";
+        std::cin >> min >> max;
+        std::uniform_int_distribution<> distr(min, max); 
+
+        for (int i = 0; i < n; ++i) {
+            arr[i] = distr(gen); 
+        }
+        std::cout << "Array:\n";
+        for (int i = 0; i < n; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "Xaxaxaxaxaxa" << std::endl;
+        delete[] arr;
+        return 1;
     }
     int MIN = arr[0];
     int IND = 0;
@@ -17,8 +45,8 @@ int main() {
             IND = i;
         }
     }
-    std::cout << "Min:" << MIN << std::endl;
-    std::cout << "Index:" << IND << std::endl;
+    std::cout << "Min: " << MIN << std::endl;
+    std::cout << "Index: " << IND << std::endl;
     int firstNegativeIndex = -1;
     int lastNegativeIndex = -1;
     for (int i = 0; i < n; ++i) {
@@ -34,7 +62,7 @@ int main() {
         for (int i = firstNegativeIndex + 1; i < lastNegativeIndex; ++i) {
             sum += arr[i];
         }
-        std::cout << "Amount:" << sum << std::endl;
+        std::cout << "Amount: " << sum << std::endl;
     } else {
         std::cout << "-=0" << std::endl;
     }
@@ -50,7 +78,7 @@ int main() {
             j++;
         }
     }
-    std::cout << "New:";
+    std::cout << "New: ";
     for (int i = 0; i < n; ++i) {
         std::cout << arr[i] << " ";
     }
